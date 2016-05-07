44e686bfb8f76fea0bc88b0ad13ab66bd35073fe
gh0st <zakne0ne@gmail.com>
2016-05-07 13:49:32 +0700

    just a simple project euler problem, yeeaahoo!

---
+++ multiples of 3 and 5.c
@@ -0,0 +1,21 @@
+/*
+Copyright © 2016 g0st
+This work is free. You can redistribute it and/or modify it under the
+terms of the Do What The Fuck You Want To Public License, Version 2,
+as published by Sam Hocevar. See the COPYING file for more details.
+
+Author: gh0st
+Favorite music composer: Bach
+*/
+#include <stdio.h>
+
+int main() {
+    int sum = 0;
+    for (int i = 0; i < 10; i++) {
+        if (i % 3 == 0 || i % 5 == 0)
+            sum += i;
+    }
+    
+    printf("%d", sum);
+    return 0;
+}
