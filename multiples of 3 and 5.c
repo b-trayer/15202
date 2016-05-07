ed782a3ba21b1773ee13528861032d88337dcfd0
gh0st <zakne0ne@gmail.com>
2016-05-07 13:55:35 +0700

    Small fix

--- multiples of 3 and 5.c
+++ multiples of 3 and 5.c
@@ -11,7 +11,7 @@ Favorite music composer: Bach
 
 int main() {
     int sum = 0;
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < 1000; i++) {
         if (i % 3 == 0 || i % 5 == 0)
             sum += i;
     }
